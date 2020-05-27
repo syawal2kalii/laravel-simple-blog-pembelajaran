21f1e341309928a57f2970aedb3a06518a61f821
syawal2kalii <syawal20162016@gmail.com>
2020-05-27 16:08:33 +0800

    hello world

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// return string
+Route::get('/hello', function (){
+    return "hello world";
+});
+
+// return view (html)
+Route::get('/hello-html', function (){
+    return view('helloworld');
+});
+
