41317b1b27a7ec0af941f3082d89f2ee8563b001
syawal2kalii <syawal20162016@gmail.com>
2020-05-29 13:28:17 +0800

    6_model

---
+++ app/Article.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    protected $guarded = ['id']; // blacklist field / memproteksi field tertentu
+//    protected $fillable = []; // whitelist field / membolehkan field tertentu
+}
